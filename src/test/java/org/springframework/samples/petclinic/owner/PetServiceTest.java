dee8a95c1ff151fefe7bb8d880a3b5bf6708162d
beafshar <baharafshar2079@gmail.com>
2021-10-15 18:37:42 +0330

    fixed some bugs

--- src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
@@ -2,56 +2,64 @@ package org.springframework.samples.petclinic.owner;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.springframework.samples.petclinic.utility.PetTimedCache;
-import org.springframework.samples.petclinic.utility.SimpleDI;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.*;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.*;
-import org.junit.runners.Parameterized.Parameters;
+import org.springframework.samples.petclinic.utility.PetTimedCache;
+
 
 import java.util.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith (Parameterized.class)
-class PetServiceTest {
-	private static PetService PetSer;
+public class PetServiceTest {
+	private PetService PetSer;
 	private static Pet dog, cat, horse, hen;
 	private final int id;
 	private final Pet pet;
+	private PetTimedCache PetCache;
 
 	public PetServiceTest(int id, Pet pet) {
 		this.id = id;
 		this.pet = pet;
+		PetCache = mock(PetTimedCache.class);
+		OwnerRepository owners = mock(OwnerRepository.class);
+		Logger logger = mock(Logger.class);
+		PetSer = new PetService(PetCache, owners, logger);
 	}
 
 	@Before
 	public void setup() {
-		SetPet();
-		PetSer = mock((PetService.class));
-		when(PetSer.findPet(1)).thenReturn(dog);
-		when(PetSer.findPet(2)).thenReturn(horse);
-		when(PetSer.findPet(3)).thenReturn(cat);
-		when(PetSer.findPet(4)).thenReturn(hen);
+//		SetPet();
+		when(PetCache.get(1)).thenReturn(dog);
+		when(PetCache.get(2)).thenReturn(horse);
+		when(PetCache.get(3)).thenReturn(cat);
+		when(PetCache.get(4)).thenReturn(hen);
+
 	}
 
-	public void SetPet() {
+	public static void SetPet() {
 		dog = new Pet();
 		dog.setId(1);
+		dog.setName("dog");
 
 		horse = new Pet();
 		horse.setId(2);
+		horse.setName("horse");
 
 		cat = new Pet();
 		cat.setId(3);
+		cat.setName("cat");
 
 		hen = new Pet();
 		hen.setId(4);
+		hen.setName("hen");
 	}
 
 	@After
@@ -61,15 +69,15 @@ class PetServiceTest {
 
 	@Parameterized.Parameters
 	public static Collection<Object[]> parameters() {
+		SetPet();
 		return Arrays.asList (new Object [][]{
-			{1, dog}, {2, horse}, {3, cat}, {4, hen}
+			{1, dog}, {2, horse}, {3, cat}, {4, hen}, {5, null}
 		});
 	}
 	@Test
 	public void FindPetTest(){
 		Pet found_pet = PetSer.findPet(id);
 		assertEquals(pet, found_pet);
-		assertNotNull(found_pet);
 	}
 
 
