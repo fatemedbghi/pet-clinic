6d8862b59cdad13d9ab078c224f37bd4e015a6d0
fatemedbghi <fatemehdbghiii@gmail.com>
2021-11-27 13:20:17 +0330

    add cutpnfp tests for line 14

--- src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
@@ -22,6 +22,59 @@ class TriCongruenceTest {
 		Assertions.assertFalse(areCongruent);
 	}
 
+	/*
+	part1 line 14
+	a = t1arr[0] != t2arr[0]
+	b = t1arr[1] != t2arr[1]
+	c = t1arr[2] != t2arr[2]
+	p -> a + b + c
+	CUTPNFP -> Corresponding unique true points and near false points
+	unique true points -> {TFF,FTF,FFT}
+	near false points -> {FFF}
+	 */
+
+	@Test
+	public void TestAreCongruentTFF()
+	{
+		Triangle t1 = new Triangle(1, 4, 4);
+		Triangle t2 = new Triangle(2, 4, 4);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+	@Test
+	public void TestAreCongruentFTF()
+	{
+		Triangle t1 = new Triangle(2, 4, 4);
+		Triangle t2 = new Triangle(2, 3, 4);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+	@Test
+	public void TestAreCongruentFFT()
+	{
+		Triangle t1 = new Triangle(2, 4, 4);
+		Triangle t2 = new Triangle(2, 4, 5);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+	@Test
+	public void TestAreCongruentFFF()
+	{
+		Triangle t1 = new Triangle(2, 4, 5);
+		Triangle t2 = new Triangle(2, 4, 5);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertTrue(areCongruent);
+	}
+
+
+
 	/**
 	 * TODO
 	 * explain your answer here
