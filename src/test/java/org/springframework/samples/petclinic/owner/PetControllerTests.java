5b12c6d1922e6943ec5b6c52733d339d92890dde
beafshar <baharafshar2079@gmail.com>
2021-12-09 07:51:48 +0330

    tests debugged succesfully!

--- src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
@@ -1,24 +1,23 @@
 package org.springframework.samples.petclinic.owner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.BDDMockito.given;
 import org.assertj.core.util.Lists;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.utility.PetTimedCache;
+
+
 
 
 @WebMvcTest(value = PetController.class,
@@ -30,11 +29,11 @@ import org.springframework.samples.petclinic.utility.PetTimedCache;
 	}
 )
 
-class PetControllerTests {
 
+class PetControllerTests {
 	private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
 	@Autowired
-	private MockMvc mockMvc;
+	private MockMvc mvc;
 
 	@MockBean
 	private PetRepository pets;
@@ -47,34 +46,37 @@ class PetControllerTests {
 	private Pet pet;
 
 	private static final int OWNER_ID = 1;
-	private static final int PETTYPE_ID = 1;
+	private static final int PETTYPE_ID = 10;
 	private static final int PET_ID = 1;
 
+
 	@BeforeEach
-	void SetUp() {
+	void setup() {
 		owner = new Owner();
 		owner.setId(OWNER_ID);
 		cat = new PetType();
 		cat.setId(PETTYPE_ID);
+		cat.setName("cat");
 		pet = new Pet();
 		pet.setId(PET_ID);
-		pet.setName("pishi");
-		given(pets.findById(PET_ID)).willReturn(pet);
-		given(owners.findById(OWNER_ID)).willReturn(owner);
 		given(pets.findPetTypes()).willReturn(Lists.newArrayList(cat));
+		given(owners.findById(OWNER_ID)).willReturn(owner);
+		given(pets.findById(PET_ID)).willReturn(pet);
 	}
+
 
 	@Test
 	public void testInitCreationFormWorksIdeal() throws Exception {
-		mockMvc.perform(get("/owners/1/pets/new").contentType(MediaType.APPLICATION_JSON))
+		mvc.perform(get("/owners/1/pets/new").contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
-			.andExpect(view().name(VIEWS_PETS_CREATE_OR_UPDATE_FORM))
-			.andExpect(model().attributeExists("pet"));
+			.andExpect(model().attributeExists("pet"))
+			.andExpect(view().name(VIEWS_PETS_CREATE_OR_UPDATE_FORM));
+
 	}
 
 	@Test
 	public void testInitUpdateFormWorksIdeal() throws Exception {
-		mockMvc.perform(get("/owners/1/pets/1/edit"))
+		mvc.perform(get("/owners/1/pets/1/edit"))
 			.andExpect(status().isOk())
 			.andExpect(view().name(VIEWS_PETS_CREATE_OR_UPDATE_FORM))
 			.andExpect(model().attributeExists("pet"));
@@ -82,7 +84,7 @@ class PetControllerTests {
 
 	@Test
 	public void testProcessCreationFormWorksIdeal() throws  Exception {
-		mockMvc.perform(post("/owners/1/pets/new")
+		mvc.perform(post("/owners/1/pets/new")
 				.param("name", "pishi")
 				.param("type", "cat")
 				.param("id", "1")
@@ -93,19 +95,19 @@ class PetControllerTests {
 	}
 	@Test
 	public void ProcessCreationFailsBecauseOfInvalidfeilds() throws  Exception {
-		mockMvc.perform(post("/owners/1/pets/new")
+		mvc.perform(post("/owners/1/pets/new")
 				.param("name", "")
-				.param("type", "cat")
+				.param("type", "")
 				.param("id", "")
 				.param("BirthDate", "2020-02-02"))
 			.andExpect(status().isOk())
-			.andExpect(view().name("CREATE_OR_UPDATE_FORM"))
+			.andExpect(view().name(VIEWS_PETS_CREATE_OR_UPDATE_FORM))
 			.andExpect(model().attributeExists("pet"));
 	}
 
 	@Test
 	public void testProcessUpdateFormWorksIdeal() throws  Exception {
-		mockMvc.perform(post("/owners/1/pets/1/edit")
+		mvc.perform(post("/owners/1/pets/1/edit")
 				.param("name", "pishi")
 				.param("type", "cat")
 				.param("id", "1")
@@ -116,14 +118,15 @@ class PetControllerTests {
 	}
 	@Test
 	public void ProcessUpdateFailswithError() throws  Exception {
-		mockMvc.perform(post("/owners/1/pets/1/edit")
+		mvc.perform(post("/owners/1/pets/1/edit")
 				.param("name", "sag")
 				.param("type", "dog")
 				.param("id", "2")
 				.param("BirthDate", "2020-02-02"))
 			.andExpect(status().isOk())
-			.andExpect(view().name("CREATE_OR_UPDATE_FORM"))
-			.andExpect(model().attributeExists("pet"));
+			.andExpect(model().attributeExists("pet"))
+			.andExpect(view().name(VIEWS_PETS_CREATE_OR_UPDATE_FORM));
 	}
+
 }
 
